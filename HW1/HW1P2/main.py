7d65895cafe728e06c057e9007eb39324644caea
Zongyue Zhao <zongyue.zhao.97@gmail.com>
2021-02-09 16:09:34 -0500

    Demo

--- HW1/HW1P2/main.py
+++ HW1/HW1P2/main.py
@@ -4,22 +4,18 @@ import torch
 import torch.nn as nn
 import torch.utils.data
 from torch.utils.data.dataset import T_co
+from torch.utils.tensorboard import SummaryWriter
 
 data_dir = 'E:/11785_data/HW1'
+
+device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
 
 
 class HyperParameters:
     context_K = 11
     batch_size = 12
     lr = 1e-3
-
-
-class MLP(nn.Module):
-    def __init__(self):
-        super(MLP, self).__init__()
-
-    def forward(self):
-        pass
+    max_epoch = 100000
 
 
 class TrainSet(torch.utils.data.Dataset):
@@ -71,6 +67,8 @@ class TestSet(torch.utils.data.Dataset):
 
 
 def main():
+    writer = SummaryWriter('./logs', comment=str(HyperParameters))
+
     train_X = np.load(os.path.join(data_dir, 'train.npy'), allow_pickle=True)  # N*?*40
     train_Y = np.load(os.path.join(data_dir, 'train_labels.npy'), allow_pickle=True)  # N*?
 
@@ -86,6 +84,58 @@ def main():
 
     test_loader = torch.utils.data.DataLoader(
         TestSet(test_X), batch_size=HyperParameters.batch_size, shuffle=False)
+
+
+def train(train_loader, valid_loader, writer):
+    model = nn.Sequential(nn.Linear(40, 64), nn.BatchNorm1d(64), nn.ReLU(),
+                          nn.Linear(64, 128), nn.BatchNorm1d(128), nn.ReLU(),
+                          nn.Linear(128, 256), nn.BatchNorm1d(256), nn.ReLU(),
+                          nn.Linear(256, 128), nn.BatchNorm1d(128), nn.ReLU(),
+                          nn.Linear(128, 71)).cuda()
+
+    model.double()
+    optimizer = torch.optim.Adam(model.parameters(), lr=HyperParameters.lr)
+    criterion = nn.CrossEntropyLoss().cuda()
+
+    with torch.cuda.device(0):
+        model.train()
+        for epoch in range(HyperParameters.max_epoch):
+            for bx, by in train_loader:
+                bx = bx.to(device)
+                by = by.to(device)
+
+                prediction = model(bx)
+                loss = criterion(prediction, by)
+
+                writer.add_scalar('Loss/Train', loss, epoch)
+
+                optimizer.zero_grad()
+                loss.backward()
+                optimizer.step()
+
+                if epoch % 1000 == 0:
+                    torch.save({
+                        'epoch': epoch,
+                        'model_state_dict': model.state_dict(),
+                        'optimizer_state_dict': optimizer.state_dict(),
+                        'loss': loss,
+                    }, 'checkpoints/model' + str(HyperParameters) + '.tar')
+                    evaluate(valid_loader, model, criterion, epoch, writer)
+                    model.train()
+
+
+def evaluate(valid_loader, model, criterion, epoch, writer):
+    with torch.cuda.device(0):
+        with torch.no_grad():
+            model.eval()
+            for bx, by in valid_loader:
+                bx = bx.to(device)
+                by = by.to(device)
+
+                prediction = model(bx)
+                loss = criterion(prediction, by)
+
+                writer.add_scalar('Loss/Validation', loss, epoch)
 
 
 if __name__ == '__main__':
