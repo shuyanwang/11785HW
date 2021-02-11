14468d7de33a043744c81731f9d35c0e5745bf0f
Zongyue Zhao <zongyue.zhao.97@gmail.com>
2021-02-11 13:55:22 -0500

    New Experiments

--- HW1/HW1P2/main.py
+++ HW1/HW1P2/main.py
@@ -3,16 +3,23 @@ from models import mlp2
 
 
 def main():
-    params = ParamsHW1(15, 131072, max_epoch=51)
-    model = mlp2.MLP2(params.K)
-    learner = LearningHW1(params, model)
-    learner.learn()
-    del learner
+    # params = ParamsHW1(15, 131072, max_epoch=51)
+    # model = mlp2.MLP2(params.K)
+    # learner = LearningHW1(params, model)
+    # learner.learn()
+    # del learner
+    #
+    # params = ParamsHW1(15, 65536, max_epoch=51)
+    # model = mlp2.MLP2(params.K)
+    # learner = LearningHW1(params, model)
+    # learner.learn()
 
-    params = ParamsHW1(15, 65536, max_epoch=51)
-    model = mlp2.MLP2(params.K)
-    learner = LearningHW1(params, model)
-    learner.learn()
+    for k in range(10, 20):
+        params = ParamsHW1(k, 65536, max_epoch=21)
+        model = mlp2.MLP2(params.K)
+        learner = LearningHW1(params, model)
+        learner.learn()
+        del learner
 
 
 if __name__ == '__main__':
