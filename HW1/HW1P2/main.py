9569a52a8a3f714d35807565067e62bc1ffbc398
Zongyue Zhao <zongyue.zhao.97@gmail.com>
2021-02-12 01:45:07 -0500

    Update Code

--- HW1/HW1P2/main.py
+++ HW1/HW1P2/main.py
@@ -1,30 +1,40 @@
 from learninghw1 import LearningHW1, ParamsHW1
-from models.mlp4 import MLP4
+from models.model import *
 
 
 def main():
     # params = ParamsHW1(15, 131072, max_epoch=51)
-    # model = mlp2.MLP2(params.K)
+    # model = MLP2(params.K)
     # learner = LearningHW1(params, model)
     # learner.learn()
     # del learner
     #
     # params = ParamsHW1(15, 65536, max_epoch=51)
-    # model = mlp2.MLP2(params.K)
+    # model = MLP2(params.K)
     # learner = LearningHW1(params, model)
     # learner.learn()
 
     # params = ParamsHW1(15, 65536, max_epoch=31)
-    # model = mlp3.MLP3(params.K)
+    # model = MLP3(params.K)
     # learner = LearningHW1(params, model)
     # learner.learn()
     # del learner
 
-    params = ParamsHW1()
+    params = ParamsHW1(lr=1e-3)
     model = MLP4(params.K)
     learner = LearningHW1(params, model)
     learner.learn()
+
     del learner
+
+    params = ParamsHW1()
+    model = MLP4(params.K)
+    learner = LearningHW1(params, model)
+    learner.load_model(epoch=5)
+    learner.learn()
+    del learner
+
+    # change lr to 1e-3
 
 
 if __name__ == '__main__':
