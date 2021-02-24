b3f0925fa0b0b12df4194ebe596a0650ddffe623
Zongyue Zhao <zongyue.zhao.97@gmail.com>
2021-02-23 20:56:11 -0500

    dropout.py

--- HW1/HW1Bonus/handout/mytorch/dropout.py
+++ HW1/HW1Bonus/handout/mytorch/dropout.py
@@ -3,21 +3,27 @@
 
 import numpy as np
 
-class Dropout(object):
-	def __init__(self, p=0.5):
-		# Dropout probability
-		self.p = p
 
-	def __call__(self, x):
-		return self.forward(x)
+class Dropout:
+    def __init__(self, p=0.5):
+        # Dropout probability
+        self.p = p
+        self.mask = None
 
-	def forward(self, x, train = True):
-		# 1) Get and apply a mask generated from np.random.binomial
-		# 2) Scale your output accordingly
-		# 3) During test time, you should not apply any mask or scaling.
-		raise NotImplementedError
-		
-	def backward(self, delta):
-		# 1) This method is only called during trianing.
-		raise NotImplementedError
+    def __call__(self, x):
+        return self.forward(x)
 
+    def forward(self, x, train=True):
+        # 1) Get and apply a mask generated from np.random.binomial
+        # 2) Scale your output accordingly
+        # 3) During test time, you should not apply any mask or scaling.
+        if not train:
+            return x
+
+        self.mask = np.random.binomial(1, self.p, x.shape)
+
+        return x * self.mask / self.p
+
+    def backward(self, delta):
+        # 1) This method is only called during training.
+        return delta * self.mask
