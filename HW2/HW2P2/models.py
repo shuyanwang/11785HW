79f836945e39a5aed0be2f180792bc3a67accdb4
Zongyue Zhao <zongyuez@andrew.cmu.edu>
2021-03-06 20:53:25 -0500

    Implementation

--- HW2/HW2P2/models.py
+++ HW2/HW2P2/models.py
@@ -1,8 +1,9 @@
 import torch
 import torch.nn as nn
 from utils.base import Model
-from torchvision.models import resnet, vgg, squeezenet, densenet
+from torchvision.models.resnet import BasicBlock, Bottleneck
 from hw2_classification import ParamsHW2Classification
+from typing import Union, Optional
 
 
 class PerceptronLayer(nn.Module):
@@ -14,56 +15,6 @@ class PerceptronLayer(nn.Module):
 
     def forward(self, x):
         return self.layer(x)
-
-
-class ResNet101(Model):
-    def __init__(self, params):
-        super().__init__(params)
-        self.net = resnet.ResNet(resnet.Bottleneck, [3, 4, 23, 3],
-                                 num_classes=self.params.output_channels)
-
-    def forward(self, x):
-        return self.net(x)
-
-
-class ResNet34(Model):
-    def __init__(self, params):
-        super().__init__(params)
-        self.net = resnet.ResNet(resnet.BasicBlock, [3, 4, 6, 3],
-                                 num_classes=self.params.output_channels)
-
-    def forward(self, x):
-        return self.net(x)
-
-
-class ResNet18(Model):
-    def __init__(self, params):
-        super().__init__(params)
-        self.net = resnet.ResNet(resnet.BasicBlock, [2, 2, 2, 2],
-                                 num_classes=self.params.output_channels)
-
-    def forward(self, x):
-        return self.net(x)
-
-
-class VGG11BN(Model):
-    def __init__(self, params):
-        super().__init__(params)
-        self.net = vgg.VGG(
-                vgg.make_layers([64, 'M', 128, 'M', 256, 256, 'M', 512, 512, 'M', 512, 512, 'M'],
-                                True), num_classes=self.params.output_channels)
-
-    def forward(self, x):
-        return self.net(x)
-
-
-class SqueezeNet(Model):
-    def __init__(self, params):
-        super().__init__(params)
-        self.net = squeezenet.SqueezeNet(version='1_1', num_classes=self.params.output_channels)
-
-    def forward(self, x):
-        return self.net(x)
 
 
 class MLP19(Model):
@@ -87,114 +38,109 @@ class MLP19(Model):
         return self.l8(x7)
 
 
-class ResNetEmbedding(nn.Module):
+class ResNet(nn.Module):
+    """
+    Note1:
+        The architecture is from https://arxiv.org/abs/1512.03385 (ResNet) and the
+        pytorch release at https://pytorch.org/vision/0.8/_modules/torchvision/models/resnet.html
+    Note2:
+    As per course policy at https://piazza.com/class/khtqzctrciu1fp?cid=71,
+    I looked at some public resources, including:
+    https://towardsdatascience.com/an-overview-of-resnet-and-its-variants-5281e2f56035
+    https://ngc.nvidia.com/catalog/resources/nvidia:resnet_50_v1_5_for_pytorch
+    https://github.com/KaimingHe/deep-residual-networks
+    """
 
-    def __init__(self, block, layers, zero_init_residual=False,
-                 groups=1, width_per_group=64, replace_stride_with_dilation=None,
-                 norm_layer=None, dropout=0.5):
+    def __init__(self, block: Optional[Union[BasicBlock, Bottleneck]], layers, embedding=False):
         super().__init__()
-        if norm_layer is None:
-            norm_layer = nn.BatchNorm2d
-        self._norm_layer = norm_layer
+        self.embedding = embedding
 
-        self.inplanes = 64
-        self.dilation = 1
-        if replace_stride_with_dilation is None:
-            # each element in the tuple indicates if we should replace
-            # the 2x2 stride with a dilated convolution instead
-            replace_stride_with_dilation = [False, False, False]
-        if len(replace_stride_with_dilation) != 3:
-            raise ValueError("replace_stride_with_dilation should be None "
-                             "or a 3-element tuple, got {}".format(replace_stride_with_dilation))
-        self.groups = groups
-        self.base_width = width_per_group
-        self.conv1 = nn.Conv2d(3, self.inplanes, kernel_size=7, stride=2, padding=3,
-                               bias=False)
-        self.bn1 = norm_layer(self.inplanes)
-        self.relu = nn.ReLU(inplace=True)
-        self.maxpool = nn.MaxPool2d(kernel_size=3, stride=2, padding=1)
-        self.layer1 = self._make_layer(block, 64, layers[0])
-        self.d1 = nn.Dropout(dropout)
-        self.layer2 = self._make_layer(block, 128, layers[1], stride=2,
-                                       dilate=replace_stride_with_dilation[0])
-        self.d2 = nn.Dropout(dropout)
-        self.layer3 = self._make_layer(block, 256, layers[2], stride=2,
-                                       dilate=replace_stride_with_dilation[1])
-        self.d3 = nn.Dropout(dropout)
-        self.layer4 = self._make_layer(block, 512, layers[3], stride=2,
-                                       dilate=replace_stride_with_dilation[2])
-        self.avgpool = nn.AdaptiveAvgPool2d((1, 1))
+        self.conv = nn.Conv2d(3, 64, kernel_size=7, stride=2, padding=3, bias=False)
+        self.bn = nn.BatchNorm2d(64)
+        self.pool1 = nn.MaxPool2d(kernel_size=3, stride=2, padding=1)
 
-        for m in self.modules():
-            if isinstance(m, nn.Conv2d):
-                nn.init.kaiming_normal_(m.weight, mode='fan_out', nonlinearity='relu')
-            elif isinstance(m, (nn.BatchNorm2d, nn.GroupNorm)):
-                nn.init.constant_(m.weight, 1)
-                nn.init.constant_(m.bias, 0)
+        self.cin = 64
+        # The number of channels feeding into the next residual layer
+        # subject to update in residual layers: the proceeding layer takes this value
+        residual_layers = [self.residual_layer(block, 64, layers[0], 1),
+                           self.residual_layer(block, 128, layers[1], 2),
+                           self.residual_layer(block, 256, layers[2], 2),
+                           self.residual_layer(block, 512, layers[3], 2)]
 
-        # Zero-initialize the last BN in each residual branch,
-        # so that the residual branch starts with zeros, and each residual block behaves like an
-        # identity.
-        # This improves the model by 0.2~0.3% according to https://arxiv.org/abs/1706.02677
-        if zero_init_residual:
-            for m in self.modules():
-                if isinstance(m, resnet.Bottleneck):
-                    nn.init.constant_(m.bn3.weight, 0)
-                elif isinstance(m, resnet.BasicBlock):
-                    nn.init.constant_(m.bn2.weight, 0)
+        self.residual_layers = nn.ModuleList(residual_layers)  # Register the layers
 
-    def _make_layer(self, block, planes, blocks, stride=1, dilate=False):
-        norm_layer = self._norm_layer
-        downsample = None
-        previous_dilation = self.dilation
-        if dilate:
-            self.dilation *= stride
-            stride = 1
-        if stride != 1 or self.inplanes != planes * block.expansion:
-            downsample = nn.Sequential(
-                    resnet.conv1x1(self.inplanes, planes * block.expansion, stride),
-                    norm_layer(planes * block.expansion),
-            )
+        self.pool2 = nn.AdaptiveAvgPool2d((1, 1))
 
-        layers = [block(self.inplanes, planes, stride, downsample, self.groups,
-                        self.base_width, previous_dilation, norm_layer)]
-        self.inplanes = planes * block.expansion
-        for _ in range(1, blocks):
-            layers.append(block(self.inplanes, planes, groups=self.groups,
-                                base_width=self.base_width, dilation=self.dilation,
-                                norm_layer=norm_layer))
+        if not embedding:
+            self.linear = nn.Linear(block.expansion * 512, 4000)
+        else:
+            self.linear = None
+
+        #### He Initialization
+        # from https://pytorch.org/vision/0.8/_modules/torchvision/models/resnet.html
+        for module in self.modules():
+            if isinstance(module, nn.Conv2d):
+                nn.init.kaiming_normal_(module.weight, mode='fan_out', nonlinearity='relu')
+            elif isinstance(module, nn.BatchNorm2d):
+                nn.init.constant_(module.weight, 1)
+                nn.init.constant_(module.bias, 0)
+
+    def residual_layer(self, block, cout, num_blocks, stride):
+
+        #### For bottleneck blocks, we need to down-sample
+        # because we need cin to match cout for addition
+        down_sample = None
+        if stride > 1 or self.cin != cout * block.expansion:
+            down_sample = nn.Sequential(
+                    nn.Conv2d(self.cin, cout * block.expansion, kernel_size=1, stride=stride,
+                              bias=False), nn.BatchNorm2d(cout * block.expansion))
+
+        layers = [block(self.cin, cout, stride, down_sample)]
+        self.cin = cout * block.expansion
+        for _ in range(1, num_blocks):
+            layers.append(block(self.cin, cout))
 
         return nn.Sequential(*layers)
 
-    def _forward_impl(self, x):
-        # See note [TorchScript super()]
-        x = self.conv1(x)
-        x = self.bn1(x)
-        x = self.relu(x)
-        x = self.maxpool(x)
+    def forward(self, x):
+        """
+        :param x:
+        :return: If embedding, return the flattened feature vector
+        """
+        x = self.pool1(torch.relu(self.bn(self.conv(x))))
+        for residual_layer in self.residual_layers:
+            x = residual_layer(x)
 
-        x = self.layer1(x)
-        x = self.d1(x)
-        x = self.layer2(x)
-        x = self.d2(x)
-        x = self.layer3(x)
-        x = self.d3(x)
-        x = self.layer4(x)
+        x = torch.flatten(self.pool2(x), 1)
 
-        x = self.avgpool(x)
-        x = torch.flatten(x, 1)
+        if self.embedding:
+            return x
 
-        return x
+        return self.linear(x)
+
+
+class ResNet101(Model):
+    def __init__(self, params):
+        super().__init__(params)
+        self.net = ResNet(Bottleneck, [3, 4, 23, 3])
 
     def forward(self, x):
-        return self._forward_impl(x)
+        return self.net(x)
 
 
-class DenseNet121(Model):
+class ResNet34(Model):
     def __init__(self, params):
-        super(DenseNet121, self).__init__(params)
-        self.net = densenet.DenseNet(num_classes=self.params.output_channels,
-                                     drop_rate=self.params.dropout)
+        super().__init__(params)
+        self.net = ResNet(BasicBlock, [3, 4, 6, 3])
+
+    def forward(self, x):
+        return self.net(x)
+
+
+class ResNet18(Model):
+    def __init__(self, params):
+        super().__init__(params)
+        self.net = ResNet(BasicBlock, [2, 2, 2, 2])
 
     def forward(self, x):
         return self.net(x)
@@ -202,8 +148,8 @@ class DenseNet121(Model):
 
 class ResNet10(Model):
     def __init__(self, params: ParamsHW2Classification):
-        super(ResNet10, self).__init__(params)
-        self.net = resnet.ResNet(resnet.BasicBlock, [1, 1, 1, 1], self.params.output_channels)
+        super().__init__(params)
+        self.net = ResNet(BasicBlock, [1, 1, 1, 1])
 
     def forward(self, x: torch.Tensor):
         return self.net(x)
@@ -212,27 +158,16 @@ class ResNet10(Model):
 class ResNet152(Model):
     def __init__(self, params):
         super().__init__(params)
-        self.net = resnet.ResNet(resnet.Bottleneck, [3, 8, 36, 3],
-                                 num_classes=self.params.output_channels)
+        self.net = ResNet(Bottleneck, [3, 8, 36, 3])
 
     def forward(self, x):
-        return self.net(x)
-
-
-class WideResNet101(Model):
-    def __init__(self, params):
-        super(WideResNet101, self).__init__(params)
-        self.net = resnet.ResNet(width_per_group=64 * 2, num_classes=4000, block=resnet.Bottleneck,
-                                 layers=[3, 4, 23, 3])
-
-    def forward(self, x: torch.Tensor):
         return self.net(x)
 
 
 class ResNet101E(Model):
     def __init__(self, params):
         super().__init__(params)
-        self.net = ResNetEmbedding(block=resnet.Bottleneck, layers=[3, 4, 23, 3])
+        self.net = ResNet(block=Bottleneck, layers=[3, 4, 23, 3], embedding=True)
 
     def forward(self, x: torch.Tensor):
         return self.net(x)
@@ -241,7 +176,7 @@ class ResNet101E(Model):
 class ResNet18E(Model):
     def __init__(self, params):
         super().__init__(params)
-        self.net = ResNetEmbedding(block=resnet.BasicBlock, layers=[2, 2, 2, 2])
+        self.net = ResNet(block=BasicBlock, layers=[2, 2, 2, 2], embedding=True)
 
     def forward(self, x: torch.Tensor):
         return self.net(x)
@@ -250,7 +185,7 @@ class ResNet18E(Model):
 class ResNet34E(Model):
     def __init__(self, params):
         super().__init__(params)
-        self.net = ResNetEmbedding(resnet.BasicBlock, [3, 4, 6, 3])
+        self.net = ResNet(BasicBlock, [3, 4, 6, 3], embedding=True)
 
     def forward(self, x):
         return self.net(x)
