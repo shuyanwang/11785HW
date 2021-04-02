eedb7f33eeee13c4559229f25e7d2558ea00d310
zongyuez <zongyuez@andrew.cmu.edu>
2021-04-02 18:52:08 -0400

    Working

--- HW3/HW3P2/hw3.py
+++ HW3/HW3P2/hw3.py
@@ -169,14 +169,14 @@ class HW3(Learning):
                 # total_acc = torch.zeros(1, device=self.device)
                 for i, batch in enumerate(tqdm(self.train_loader)):
                     x = batch[0].to(self.device)
-                    lengths_x = tuple(batch[1])
+                    lengths_x = batch[1]
                     y = batch[2].to(self.device)
-                    lengths_y = tuple(batch[3])
+                    lengths_y = batch[3]
 
                     # (T,N,C)
-                    output, _ = self.model(x, lengths_x)
+                    output, lengths_out = self.model(x, lengths_x)
 
-                    loss = self.criterion(output, y, lengths_x, lengths_y)
+                    loss = self.criterion(output, y, lengths_out, lengths_y)
                     total_loss += loss
 
                     self.optimizer.zero_grad()
@@ -209,14 +209,14 @@ class HW3(Learning):
 
                 for i, batch in enumerate(self.valid_loader):
                     x = batch[0].to(self.device)
-                    lengths_x = tuple(batch[1])
+                    lengths_x = batch[1]
                     y = batch[2].to(self.device)
-                    lengths_y = tuple(batch[3])
+                    lengths_y = batch[3]
 
                     # (T,B,C)
                     output, lengths_out = self.model(x, lengths_x)
 
-                    loss = self.criterion(output, y, lengths_x, lengths_y)
+                    loss = self.criterion(output, y, lengths_out, lengths_y)
                     total_loss += loss
 
                     y_strs = HW3.to_str(y, lengths_y)
@@ -335,158 +335,6 @@ def main():
         learner.train(checkpoint_interval=args.save)
     if args.test:
         learner.test()
-
-
-# def greedy_search(symbols, logits):
-#     """
-#
-#     :param symbols:
-#     :param logits: (T,42)
-#     :return:
-#     """
-#     path = []
-#     end_with_blank = False
-#     p = 0
-#     for t in range(logits.shape[0]):
-#         p += torch.max(logits[t, :])
-#         index = torch.argmax(logits[t, :])
-#         if index != 0:
-#             if end_with_blank:
-#                 # path.append(symbols[0])
-#                 path.append(symbols[index])
-#                 end_with_blank = False
-#             else:
-#                 if len(path) == 0 or path[-1] != symbols[index]:
-#                     path.append(symbols[index])
-#         else:
-#             end_with_blank = True
-#
-#     return ''.join(path), p
-#
-#
-# class BeamSearchClass:
-#     def __init__(self, symbols, k):
-#         self.symbols = symbols
-#         self.k = k
-#
-#     def decode(self, logits):
-#         self.y_probs = logits
-#         self.paths_blank: List[str] = ['']
-#         self.paths_blank_score: Dict[str:np.ndarray] = {'': logits[0, 0]}
-#
-#         self.paths_symbol: List[str] = [c for c in self.symbols]
-#         self.paths_symbol_score: Dict[str:np.ndarray] = {}
-#         for i, c in enumerate(self.symbols):
-#             self.paths_symbol_score[c] = logits[0, i]
-#
-#         for t in range(1, self.y_probs.shape[0]):
-#             self.prune()
-#             updated_paths_symbol, updated_paths_symbol_score = self.extend_with_symbol(t)
-#             updated_paths_blank, updated_paths_blank_score = self.extend_with_blank(t)
-#             self.paths_blank = updated_paths_blank
-#             self.paths_symbol = updated_paths_symbol
-#             self.paths_blank_score = updated_paths_blank_score
-#             self.paths_symbol_score = updated_paths_symbol_score
-#
-#         return self.merge()
-#
-#     def extend_with_symbol(self, t):
-#         updated_paths_symbol = []
-#         updated_paths_symbol_score = {}
-#
-#         for path in self.paths_blank:
-#             for i, c in enumerate(self.symbols):
-#                 new_path = path + c
-#                 updated_paths_symbol.append(new_path)
-#                 updated_paths_symbol_score[new_path] = self.paths_blank_score[path] +
-#                 self.y_probs[
-#                     t, i]
-#
-#         for path in self.paths_symbol:
-#             for i, c in enumerate(self.symbols):
-#                 new_path = path if c == path[-1] else path + c
-#                 if new_path in updated_paths_symbol_score:
-#                     updated_paths_symbol_score[new_path] += self.paths_symbol_score[path] + \
-#                                                             self.y_probs[t, i]
-#                 else:
-#                     updated_paths_symbol_score[new_path] = self.paths_symbol_score[path] + \
-#                                                            self.y_probs[t, i]
-#                     updated_paths_symbol.append(new_path)
-#
-#         return updated_paths_symbol, updated_paths_symbol_score
-#
-#     def extend_with_blank(self, t):
-#         updated_paths_blank = []
-#         updated_paths_blank_score = {}
-#
-#         for path in self.paths_blank:
-#             updated_paths_blank.append(path)
-#             updated_paths_blank_score[path] = self.paths_blank_score[path] + self.y_probs[t, 0]
-#
-#         for path in self.paths_symbol:
-#             if path in updated_paths_blank:
-#                 updated_paths_blank_score[path] += self.paths_symbol_score[path] + self.y_probs[
-#                     t, 0]
-#             else:
-#                 updated_paths_blank_score[path] = self.paths_symbol_score[path] + self.y_probs[
-#                 t, 0]
-#                 updated_paths_blank.append(path)
-#
-#         return updated_paths_blank, updated_paths_blank_score
-#
-#     def prune(self):
-#         updated_paths_blank = []
-#         updated_paths_blank_score = {}
-#
-#         updated_paths_symbol = []
-#         updated_paths_symbol_score = {}
-#
-#         scores = []
-#
-#         for score in self.paths_blank_score.values():
-#             scores.append(score)
-#
-#         for score in self.paths_symbol_score.values():
-#             scores.append(score)
-#
-#         scores.sort()
-#
-#         cutoff = scores[-1] if len(scores) < self.k else scores[- self.k]
-#
-#         for path in self.paths_blank:
-#             if self.paths_blank_score[path] >= cutoff:
-#                 updated_paths_blank.append(path)
-#                 updated_paths_blank_score[path] = self.paths_blank_score[path]
-#
-#         for path in self.paths_symbol:
-#             if self.paths_symbol_score[path] >= cutoff:
-#                 updated_paths_symbol.append(path)
-#                 updated_paths_symbol_score[path] = self.paths_symbol_score[path]
-#
-#         self.paths_symbol_score = updated_paths_symbol_score
-#         self.paths_symbol = updated_paths_symbol
-#         self.paths_blank_score = updated_paths_blank_score
-#         self.paths_blank = updated_paths_blank
-#
-#     def merge(self):
-#         paths = self.paths_blank
-#         scores = self.paths_blank_score
-#
-#         for path in self.paths_symbol:
-#             if path in paths:
-#                 scores[path] += self.paths_symbol_score[path]
-#             else:
-#                 paths.append(path)
-#                 scores[path] = self.paths_symbol_score[path]
-#
-#         max_path = paths[0]
-#         max_score = scores[max_path]
-#         for path in scores:
-#             if scores[path] > max_score:
-#                 max_path = path
-#                 max_score = scores[path]
-#
-#         return max_path, scores
 
 
 if __name__ == '__main__':
--- HW3/HW3P2/models.py
+++ HW3/HW3P2/models.py
@@ -9,7 +9,7 @@ class ModelHW3(nn.Module, ABC):
         self.params = params
 
     @abstractmethod
-    def forward(self, x: torch.Tensor, lengths: tuple):
+    def forward(self, x: torch.Tensor, lengths):
         """
 
         :param x: padded (B,T,C)
@@ -27,11 +27,60 @@ class Model1(ModelHW3):
                           dropout=params.dropout, bidirectional=params.bi)
         self.linear = nn.Linear(params.hidden_size, params.output_channels)
 
-    def forward(self, x: torch.Tensor, lengths: tuple):
+    def forward(self, x: torch.Tensor, lengths):
         x = self.conv1(torch.transpose(x, 1, 2))
         x = torch.relu(x)
         x = torch.transpose(x, 1, 2)
         x = nn.utils.rnn.pack_padded_sequence(x, lengths, batch_first=True, enforce_sorted=False)
+        x = self.rnn(x)[0]
+        x, out_lengths = nn.utils.rnn.pad_packed_sequence(x)
+        if self.params.bi:
+            x = x[:, :, :self.params.hidden_size] + x[:, :, self.params.hidden_size:]
+
+        x = torch.relu(x)
+        x = self.linear(x)
+        x = torch.log_softmax(x, 2)
+        return x, out_lengths
+
+
+class Model2(ModelHW3):
+    def __init__(self, params):
+        super().__init__(params)
+        self.conv1 = nn.Conv1d(params.input_dims[0], params.conv_size, kernel_size=1)
+        self.rnn = nn.LSTM(params.conv_size, params.hidden_size, params.num_layer, batch_first=True,
+                           dropout=params.dropout, bidirectional=params.bi)
+        self.linear = nn.Linear(params.hidden_size, params.output_channels)
+
+    def forward(self, x: torch.Tensor, lengths):
+        x = self.conv1(torch.transpose(x, 1, 2))
+        x = torch.relu(x)
+        x = torch.transpose(x, 1, 2)
+        x = nn.utils.rnn.pack_padded_sequence(x, lengths, batch_first=True, enforce_sorted=False)
+        x = self.rnn(x)[0]
+        x, out_lengths = nn.utils.rnn.pad_packed_sequence(x)
+        if self.params.bi:
+            x = x[:, :, :self.params.hidden_size] + x[:, :, self.params.hidden_size:]
+
+        x = torch.relu(x)
+        x = self.linear(x)
+        x = torch.log_softmax(x, 2)
+        return x, out_lengths
+
+
+class Model3(ModelHW3):
+    def __init__(self, params):
+        super().__init__(params)
+        self.conv1 = nn.Conv1d(params.input_dims[0], params.conv_size, kernel_size=3)
+        self.rnn = nn.GRU(params.conv_size, params.hidden_size, params.num_layer, batch_first=True,
+                          dropout=params.dropout, bidirectional=params.bi)
+        self.linear = nn.Linear(params.hidden_size, params.output_channels)
+
+    def forward(self, x: torch.Tensor, lengths):
+        x = self.conv1(torch.transpose(x, 1, 2))
+        x = torch.relu(x)
+        x = torch.transpose(x, 1, 2)
+        lengths_x = lengths - 2
+        x = nn.utils.rnn.pack_padded_sequence(x, lengths_x, batch_first=True, enforce_sorted=False)
         x = self.rnn(x)[0]
         x, out_lengths = nn.utils.rnn.pad_packed_sequence(x)
         if self.params.bi:
