05da4e4d89813810aff1c106e43242644d7e7c97
Zongyue Zhao <zongyuez@andrew.cmu.edu>
2021-04-26 00:23:23 -0400

    Mean

--- HW2/HW2P1Bonus/mytorch/pool.py
+++ HW2/HW2P1Bonus/mytorch/pool.py
@@ -67,6 +67,8 @@ class MeanPoolLayer:
     def __init__(self, kernel, stride):
         self.kernel = kernel
         self.stride = stride
+        self.in_w = None
+        self.in_h = None
 
     def __call__(self, x):
         return self.forward(x)
@@ -78,7 +80,21 @@ class MeanPoolLayer:
         Return:
             out (np.array): (batch_size, out_channel, output_width, output_height)
         """
-        raise NotImplementedError
+        self.in_w, self.in_h = x.shape[2], x.shape[3]
+        out_w, out_h = (self.in_w - self.kernel) // self.stride + 1, (
+                self.in_h - self.kernel) // self.stride + 1
+
+        out = np.zeros((x.shape[0], x.shape[1], out_w, out_h))
+
+        for b in range(x.shape[0]):
+            for c in range(x.shape[1]):
+                for col in range(out_w):
+                    for row in range(out_h):
+                        out[b, c, col, row] = np.mean(
+                                x[b, c, col * self.stride:col * self.stride + self.kernel,
+                                row * self.stride:row * self.stride + self.kernel])
+
+        return out
 
     def backward(self, delta):
         """
@@ -87,4 +103,15 @@ class MeanPoolLayer:
         Return:
             dx (np.array): (batch_size, in_channel, input_width, input_height)
         """
-        raise NotImplementedError
+        B, C = delta.shape[0], delta.shape[1]
+        dx = np.zeros((B, C, self.in_w, self.in_h))
+        for b in range(B):
+            for c in range(C):
+                for col in range(delta.shape[2]):
+                    for row in range(delta.shape[3]):
+                        for k1 in range(self.kernel):
+                            for k2 in range(self.kernel):
+                                dx[b, c, col * self.stride + k1, row * self.stride + k2] += delta[
+                                                                                                b, c, col, row] / self.kernel / self.kernel
+
+        return dx
