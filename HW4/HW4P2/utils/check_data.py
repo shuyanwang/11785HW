9dccc0e059e7de26ab9465c75b24d22b0b70163b
Zongyue Zhao <zongyuez@andrew.cmu.edu>
2021-04-26 01:19:05 -0400

    Init

--- HW4/HW4P2/utils/check_data.py
+++ HW4/HW4P2/utils/check_data.py
@@ -1,12 +1,15 @@
 import numpy as np
 import os
 
-data_dir = 'E:/11785_data/HW1'
+data_dir = 'c:/DLData/11785_data/HW4'
 
-dev_x = np.load(os.path.join(data_dir, 'dev.npy'), allow_pickle=True)
+train_x = np.load(os.path.join(data_dir, 'train.npy'), allow_pickle=True)  # (N,T_in,40)
 
-t = dev_x[5]
+a = train_x[0]
 
-train_y = np.load(os.path.join(data_dir, 'train_labels.npy'), allow_pickle=True)
+train_y = np.load(os.path.join(data_dir, 'train_transcripts.npy'),
+                  allow_pickle=True)  # (N,T_out), dtype=bytes
+
+b = train_y[0][0]
 
 t = 1
